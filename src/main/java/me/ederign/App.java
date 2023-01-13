c2b16d2263524b0fa89119ddfc1f7020d71ccf5c
Eder Ignatowicz <ignatowicz@gmail.com>
2023-01-12 20:38:55 -0500

    project creation

---
+++ src/main/java/me/ederign/App.java
@@ -0,0 +1,15 @@
+package me.ederign;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int oi(){
+        return -1;
+    }
+}
